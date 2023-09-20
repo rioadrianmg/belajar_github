74c397dc90bde04307f4539ba804e8f3e5181fe5
rioadrianmg <145081254+rioadrianmg@users.noreply.github.com>
2023-09-20 15:06:52 +0700

    Create biodata.kt

---
+++ biodata.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+    var nama  = "Rio Adrian M G"
+    var kelas = "XI RPL 2"
+    var hobi  = "Write Code"
+
+    println("Nama: $nama")
+    println("Kelas: $kelas")
+    println("Nama: $hobi")
+}
